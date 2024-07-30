8f03d0ee05be752172603ae07ca90d6eed8c9f09
moldirerlan <112340218+moldirerlan@users.noreply.github.com>
2024-07-30 19:14:06 +0500

    add solution of task

--- codewars/Count Repeats.js
+++ codewars/Count Repeats.js
@@ -13,3 +13,19 @@
 
 
 //SOLUTION
+function countRepeats(str) {
+    let count = 0;
+    
+    for (let i = 0; i < str.length - 1; i++) {
+        if (str[i] === str[i + 1]) {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+// Examples
+console.log(countRepeats('abbbbc')); // Output: 3
+console.log(countRepeats('abbcca')); // Output: 2
+console.log(countRepeats('ab cca')); // Output: 1
